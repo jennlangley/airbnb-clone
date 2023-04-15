c6fc2c5f1365d06b16d485b02814d160f82b9c1c
Jennifer Langley <jlangley1019@gmail.com>
2023-04-15 18:33:04 -0400

    added Spot model

---
+++ backend/db/models/spot.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    static associate(models) {
+      Spot.belongsTo(models.User, {foreignKey: 'ownerId'})
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER
+    },
+    address: {
+      type: DataTypes.STRING
+    },
+    city: {
+      type: DataTypes.STRING
+    },
+    state: {
+      type: DataTypes.STRING
+    },
+    country: {
+      type: DataTypes.STRING
+    },
+    lat: {
+      type: DataTypes.DECIMAL
+    },
+    lng: {
+      type: DataTypes.DECIMAL
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
