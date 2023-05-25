50968bf94ddbf514c1b668c5b4153a5644799a37
Jennifer Langley <jlangley1019@gmail.com>
2023-05-25 19:33:12 -0400

    login form added

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from '../../store/session';
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import './LoginForm.css';
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+   
+    if (sessionUser) return <Redirect to='/' />;
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors({});
+        return dispatch(sessionActions.setSession({
+            credential: credential,
+            password: password
+        })).catch(
+            async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            }
+        );
+    };
+
+    return (
+        <>
+            <h1>Log in</h1>
+            <form
+            onSubmit={handleSubmit}
+            >
+                <label>Username or Email
+                    <input
+                    type='text'
+                    value={credential}
+                    onChange={e => setCredential(e.target.value)}
+                    required
+                    />
+                </label>
+                <label>Password
+                    <input
+                    type='password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                    />
+                </label>
+                {errors.credential && <p>{errors.credential}</p>}
+                <button type='submit'>Log In</button>
+            </form>
+        </>
+    );
+};
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,45 @@
+import { csrfFetch } from "./csrf";
+
+const SET_SESSION = 'session/setSession';
+const REMOVE_SESSION = 'session/removeSession';
+
+export const setSessionAction = (user) => {
+    return {
+        type: SET_SESSION,
+        user
+    };
+};
+
+export const removeSessionAction = () => {
+    return {
+        type: REMOVE_SESSION,
+    }
+};
+
+export const setSession = (user) => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify(user)
+    })
+    if (response.ok) {
+        const session = await response.json()
+        dispatch(setSessionAction(user))
+        return session;
+    }
+};
+
+const sessionReducer = (state = {user: null}, action) => {
+    let newState = { ...state }
+    switch (action.type) {
+        case SET_SESSION:
+            newState.user = action.user;
+            return newState;
+        case REMOVE_SESSION:
+            newState.user = null;
+            return newState;
+        default:
+            return newState;
+    }
+};
+
+export default sessionReducer;
